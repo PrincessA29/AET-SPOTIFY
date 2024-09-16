b84513b36abd454a7866729641f0b82a981b37c4
Princess Joeby Arabis <pjarabis2022@plm.edu.ph>
2024-09-16 21:28:02 +0800

    AET

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  constructor() {}
+
+  logResult(ev: { detail: any }) {
+    console.log(JSON.stringify(ev.detail, null, 2));
+  }
+}
+
